ddeffb442601cb4ca7b8f3d396262b4c418193f2
Maxime Méloux <maxime.meloux@ensimag.grenoble-inp.fr>
2015-12-03 20:30:22 +0100

    Utilities

---
+++ js/utilities.js
@@ -0,0 +1,18 @@
+/*
+ * Color Madness by pie3636, 2015. Code under license CC BY-SA ─ https://creativecommons.org/licenses/by-sa/3.0/
+ * Full legal stuff at https://creativecommons.org/licenses/by-sa/3.0/legalcode
+ */
+
+"use strict;"
+
+function set(x, val) {
+        return (typeof x === 'undefined' ? val : x);
+}
+
+function W(i) {
+    return Math.floor(i * w);
+}
+
+function H(i) {
+    return Math.floor(i * h);
+}
