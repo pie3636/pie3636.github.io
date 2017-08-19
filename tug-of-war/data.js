2c6de284ac8383b82aba82e1c729f7bfed177970
Maxime Méloux <maxime.meloux@ensimag.grenoble-inp.fr>
2017-08-19 02:52:52 +0200

    First release of the tug of war stats

---
+++ tug-of-war/data.js
@@ -0,0 +1,127 @@
+data = {
+    charts: {
+        historical: "http://i.imgur.com/MBV3pwi.png",
+        temporal: "http://i.imgur.com/hMX3MEW.png",
+        frequency: "http://i.imgur.com/er9W7Pr.png"
+    },
+    comment: "- We have never stayed above zero for so long\n\
+              - A new stalemate record has been reached\n\
+              - The count has recently been struggling between 3,000 and 3,600\n\
+              - After a spike in activity, the main Tug of War thread seems to have become slower. This might be due to the abundance of ToW-like threads popping up",
+    counts: {
+        total:       { cur: 111635,  prev: 104055  },
+        min:         { cur: -4043,   prev: -4043   },
+        max:         { cur: 4084,    prev: 3000    },
+        range:       { cur: 8127,    prev: 7043    },
+        ofMaxRange:  13.74,
+        avg:         { cur: -19.67,  prev: -261.89 },
+        med:         { cur: 35,      prev: 10      },
+        mostCommon:  {
+            cur:  [[0, 1124], [-1, 784], [1, 712], [-1026, 539], [-1027, 517], [2, 365], [-2, 288], [3, 277], [7, 244], [6, 234]],
+            prev: [[0, 1124], [-1, 784], [1, 712], [-1026, 539], [-1027, 517], [2, 365], [-2, 288], [3, 277], [7, 244], [6, 234]]
+        },
+        leastCommon: {
+            cur:  [[-4043, 1], ["1084 and many numbers in the 1084-2900 range", 1], [4084, 1]],
+            prev: [[-4043, 1], ["1084 and many numbers in the 1084-2900 range", 1], [4084, 1]]
+        }
+    },
+    replyTime: {
+        started: { cur: 626.91, prev: 599.76, unit: "day"    },
+        fastest: { cur: 1,      prev: 0,      unit: "second" },
+        slowest: { cur: 19.86,  prev: 19.86,  unit: "day"    },
+        avg:     { cur: 8.09,   prev: 8.30,   unit: "minute" },
+        med:     { cur: 13,     prev: 13,     unit: "second" }
+    },
+    countSign: {
+        pos:           { cur: 62375,  prev: 54795 },
+        neg:           { cur: 48136,  prev: 48136 },
+        posPercent:    { cur: 55.87,  prev: 52.66 },
+        negPercent:    { cur: 43.12,  prev: 46.26 },
+        zeros:         { cur: 1124,   prev: 1124  },
+        ofZero:        { cur: 99.32,  prev: 99.32 },
+        maxPosStreak:  { cur: 15181,  prev: 7039  },
+        maxNegStreak:  { cur: 16155,  prev: 16155 },
+        maxUpStreak:   {
+            length:  { cur: 1129,   prev: 1129  },
+            percent: { cur: 13.89,  prev: 11.75 },
+            begin: -3753, end: -2624
+        },
+        maxDownStreak: {
+            length:  { cur: 571,    prev: 571   },
+            percent: { cur: 7.03,   prev: 5.95  },
+            begin: 227, end: -344
+        },
+        avgStreak:     { cur: 2.9292,   prev: 2.9776,   precision: 4 },
+        posNegAsym:    { cur: 0.002101, prev: 0.000297, precision: 6 }
+    },
+    gets: {
+        count:      { cur: 237,    prev: 237 },
+        avg:        { cur: -72.57, prev: -373.75 },
+        med:        { cur: 0,      prev: 0 },
+        mostCommon: {
+            cur:  [[200, 36], [0, 34], [400, 23], [-200, 21], [-400, 11]],
+            prev: [[200, 36], [0, 34], [400, 23], [-200, 21], [-400, 11]]
+        },
+        leastCommon: {
+            cur:  [[-4000, 1], ["1200 and most gets in the 1200-2800 range", 1]],
+            prev: [[-4000, 1], ["1200 and most gets in the 1200-2800 range", 1]]
+        },
+        fastest:    {
+            time:  { cur: 9.25,  prev: 9.25,  unit: "minute" },
+            begin: -3200, end: -3000
+        },
+        slowest:    {
+            time:  { cur: 33.39, prev: 33.39, unit: "day" },
+            begin: 400,
+            end:   200
+        },
+        avgTime:    { cur: 63.75,  prev: 66.31, unit: "hour" },
+        medTime:    { cur: 22.63,  prev: 23.25, unit: "hour" },
+        shortest:   { cur: 200,    prev: 200 },
+        longest:    {
+            length: { cur: 4144, prev: 4144 },
+            begin: -200, end: 0
+        },
+        avgLen:     { cur: 471.03, prev: 477.31 },
+        medLen:     { cur: 350,    prev: 352 }
+    },
+    getSign: {
+        pos:           { cur: 107,   prev: 88    },
+        neg:           { cur: 96,    prev: 96    },
+        posPercent:    { cur: 45.15, prev: 40.37 },
+        negPercent:    { cur: 40.51, prev: 44.02 },
+        zeros:         { cur: 34,    prev: 34    },
+        ofZero:        { cur: 6.97,  prev: 6.97  },
+        maxPosStreak:  { cur: 25,    prev: 25    },
+        maxNegStreak:  { cur: 53,    prev: 53    },
+    },
+    oneStalemates: {
+        cur:  [[-1026, 499], [-1027, 499], [-1563, 184], [-1564, 183], [-1731, 157], [-1731, 152], [-1, 110], [3527, 101], [0, 100], [3528, 95]],
+        prev: [[-1026, 499], [-1027, 499], [-1563, 184], [-1564, 183], [-1731, 157], [-1731, 152], [-1, 110], [0, 100], [0, 0], [0, 0]]
+    },
+    twoStalemates: {
+        cur:  [[-1026, -1027, 998], [-1563, -1564, 367], [-1731, -1730, 305], [-1, 0, 201], [3527, 3528, 191], [-10, -11, 159], [277, 276, 150], [156, "157 (order of the 157)", 142], [560, 561, 129], [203, 202, 120]],
+        prev: [[-1026, -1027, 998], [-1563, -1564, 367], [-1731, -1730, 305], [-1, 0, 201], [-10, -11, 159], [277, 276, 150], [156, "157 (order of the 157)", 142], [560, 561, 129], [203, 202, 120], [0, 0, 0]]
+    },
+    deletedCounts: { cur: 525, prev: 522 },
+    forks: {
+        cur: 14,
+        prev: 14,
+        data: [
+        ["https://www.reddit.com/r/counting/comments/3st17h/counting_up_to_600_or_down_to_200/cx27pre/"],
+        ["https://www.reddit.com/r/counting/comments/401le7/counting_tug_of_war_from_400_also_known_as/cyr8ach/", true],
+        ["https://www.reddit.com/r/counting/comments/401le7/counting_tug_of_war_from_400_also_known_as/cyra2k6/"],
+        ["https://www.reddit.com/r/counting/comments/4eg74h/tug_of_war_0/d22acpn/"],
+        ["https://www.reddit.com/r/counting/comments/4ik5uy/tug_of_war_0/d33s24t/"],
+        ["https://www.reddit.com/r/counting/comments/4n8tw7/tug_of_war_200/d49z9xv/"],
+        ["https://www.reddit.com/r/counting/comments/4p7rt5/tug_of_war_600/d4r1d34/"],
+        ["https://www.reddit.com/r/counting/comments/4udrhp/tug_of_war_200/d5pll5u/"],
+        ["https://www.reddit.com/r/counting/comments/4ulp7p/tug_of_war_0/d5seco8/"],
+        ["https://www.reddit.com/r/counting/comments/51dazw/tug_of_war_2200/d7eyr4d/"],
+        ["https://www.reddit.com/r/counting/comments/5h5xnk/tug_of_war_400/daz3bja/"],
+        ["https://www.reddit.com/r/counting/comments/64ew2x/tug_of_war_200/dg2aa2z/"],
+        ["https://www.reddit.com/r/counting/comments/69mu14/tug_of_war_1000/dh7xc70/"],
+        ["https://www.reddit.com/r/counting/comments/6ndwo2/tug_of_war_3400/dkc24vj/"],
+        ["https://www.reddit.com/r/counting/comments/6r5367/tug_of_war_3200/dl44sxr/"]]
+    }
+};
---
+++ tug-of-war/main.js
@@ -0,0 +1,253 @@
+var lastData = "08 Aug 2017"
+
+var lastUpdate = "08 Aug 2017"
+var announcement = true
+
+function changeTab(newTab) {
+        $("#nav_" + currentTab).parent().removeClass("active");
+        $("#nav_" + newTab).parent().addClass("active");
+        $("#" + currentTab).hide();
+        $("#" + newTab).show();
+        currentTab = newTab;
+        scroll();
+}
+
+$(function () {
+        $("#loading").remove();
+        currentTab = "charts";
+        
+        $("#version").append(lastData);
+        $("#updateAnnouncementVersion").html(lastUpdate);
+
+        $('.modal').on('show.bs.modal', centerModal);
+        $(window).on("resize", function() {
+            $('.modal:visible').each(centerModal);
+        });
+        
+        if (announcement) {
+            $("#updateAnnouncement").show();
+        }
+        $("#updateAnnouncementClose").click(function() {
+            $("#updateAnnouncement").hide();
+        });
+        
+        /******************** Data inclusion ********************/
+        
+        if (data.comment !== "") {
+            $("#comment").html(data.comment.replace(/\n/g, "<br />"));
+            $("#comment-panel").show();
+        }
+        
+        for (var i in data.charts) {
+            $("#charts-link-" + i).attr("href", data.charts[i]);
+            $("#charts-img-" + i).attr("src", data.charts[i]);
+        }
+        
+        updateValue(data.counts.total, "total-counts", true);
+        updateValue(data.counts.min, "min-count", true);
+        updateValue(data.counts.max, "max-count", true);
+        updateValue(data.counts.range, "count-range", true);
+        updateValue(data.counts.ofMaxRange, "of-max-count-range");
+        updateValue(data.counts.avg, "avg-count", true, 2);
+        updateValue(data.counts.med, "med-count", true);
+        
+        updateTable(data.counts.mostCommon, "most-common-counts");
+        updateTable(data.counts.leastCommon, "least-common-counts");
+        
+        updateValue(data.replyTime.started, "started", true, 2, true);
+        updateValue(data.replyTime.fastest, "fastest-count", true, 2, true);
+        updateValue(data.replyTime.slowest, "slowest-count", true, 2, true);
+        updateValue(data.replyTime.avg, "avg-time", true, 2, true);
+        updateValue(data.replyTime.med, "med-time", true, 0, true);
+        updateValue(data.countSign.pos, "pos-counts", true);
+        updateValue(data.countSign.neg, "neg-counts", true);
+        updateValue(data.countSign.posPercent, "pos-counts-percent", true, 2);
+        updateValue(data.countSign.negPercent, "neg-counts-percent", true, 2);
+        updateValue(data.countSign.zeros, "zero-counts", true);
+        updateValue(data.countSign.ofZero, "of-counts-zero", true, 2);
+        updateValue(data.countSign.maxPosStreak, "max-pos-streak", true);
+        updateValue(data.countSign.maxNegStreak, "max-neg-streak", true);
+        updateValue(data.countSign.maxUpStreak.length, "max-up-streak", true);
+        updateValue(data.countSign.maxUpStreak.percent, "max-up-streak-percent", true, 2);
+        updateValue(data.countSign.maxUpStreak.begin, "max-up-streak-begin");
+        updateValue(data.countSign.maxUpStreak.end, "max-up-streak-end");
+        updateValue(data.countSign.maxDownStreak.length, "max-down-streak", true);
+        updateValue(data.countSign.maxDownStreak.percent, "max-down-streak-percent", true, 2);
+        updateValue(data.countSign.maxDownStreak.begin, "max-down-streak-begin");
+        updateValue(data.countSign.maxDownStreak.end, "max-down-streak-end");
+        updateValue(data.countSign.avgStreak, "avg-streak", true);
+        updateValue(data.countSign.posNegAsym, "pos-neg-asym", true);
+        updateValue(data.gets.count, "total-gets", true);
+        updateValue(data.gets.avg, "avg-get", true, 2);
+        updateValue(data.gets.med, "med-get", true);
+        
+        updateTable(data.gets.mostCommon, "most-common-gets");
+        updateTable(data.gets.leastCommon, "least-common-gets");
+        
+        updateValue(data.gets.fastest.time, "fastest-get", true, 2, true);
+        $("#fastest-get-direction").html(data.gets.fastest.begin < data.gets.fastest.end ? "up" : "down")
+        updateValue(data.gets.fastest.begin, "fastest-get-begin");
+        updateValue(data.gets.fastest.end, "fastest-get-end");
+        updateValue(data.gets.slowest.time, "slowest-get", true, 2, true);
+        $("#slowest-get-direction").html(data.gets.slowest.begin < data.gets.slowest.end ? "up" : "down")
+        updateValue(data.gets.slowest.begin, "slowest-get-begin");
+        updateValue(data.gets.slowest.end, "slowest-get-end");
+        updateValue(data.gets.avgTime, "avg-get-time", true, 2, true);
+        updateValue(data.gets.medTime, "med-get-time", true, 2, true);
+        updateValue(data.gets.longest.length, "longest-get", true, 2, true);
+        $("#longest-get-direction").html(data.gets.longest.begin < data.gets.longest.end ? "up" : "down")
+        updateValue(data.gets.longest.begin, "longest-get-begin");
+        updateValue(data.gets.longest.end, "longest-get-end");
+        updateValue(data.gets.avgLen, "avg-get-len", true, 2);
+        updateValue(data.gets.medLen, "med-get-len", true);
+        updateValue(data.getSign.pos, "pos-gets", true);
+        updateValue(data.getSign.neg, "neg-gets", true);
+        updateValue(data.getSign.posPercent, "pos-gets-percent", true, 2);
+        updateValue(data.getSign.negPercent, "neg-gets-percent", true, 2);
+        updateValue(data.getSign.zeros, "zero-gets", true);
+        updateValue(data.getSign.ofZero, "of-gets-zero", true, 2);
+        updateValue(data.getSign.maxPosStreak, "max-pos-get-streak", true);
+        updateValue(data.getSign.maxNegStreak, "max-neg-get-streak", true);
+        
+        updateTable(data.oneStalemates, "one-stalemates");
+        updateTable2(data.twoStalemates, "two-stalemates");
+        
+        updateValue(data.deletedCounts, "deleted-counts", true);
+        updateValue(data.forks, "forks", true);
+});
+
+function updateValue(value, id, hasVariation, digits, hasUnit) {
+    if (hasVariation) {
+        valueCur = value.cur;
+        variation = valueCur - value.prev;
+    } else {
+        valueCur = value;
+        variation = 0;
+    }
+    $("#" + id).html(format(valueCur));
+    if (hasVariation && variation) {
+        isPos = variation > 0;
+        if (typeof digits !== "undefined" && digits !== 0 || typeof (digits = value.precision) !== "undefined") { // Try to read from parameter, if it fails try to read from JSON
+            variation = variation.toFixed(digits);
+            if (~variation.indexOf(".")) { // remove trailing zeroes
+                while (variation[variation.length - 1] === "0") {
+                    variation = variation.slice(0, -1);
+                }
+                if (variation.slice(-1) === ".") { // remove trailing "."
+                    variation = variation.slice(0, -1);
+                }
+            }
+        }
+        variationStr = format(String(variation).replace("-", "")) + "</span>";
+        if (isPos) {
+            variationStr = "<span class='green'>+ " + variationStr;
+        } else {
+            variationStr = "<span class='red'>- " + variationStr;
+        }
+        $("#" + id + "-variation").html(" (" + variationStr + ")");
+    }
+    if (hasUnit) {
+        $("#" + id + "-unit").html(value.unit + (valueCur == 0 || valueCur == 1 ? "" : "s"));
+    }
+}
+
+function updateTable(values, id) {
+    for (value in values.cur) {
+        var isNew = "New";
+        for (value2 in values.prev) {
+            if (values.cur[value][0] === values.prev[value2][0] && values.cur[value][1] === values.prev[value2][1]) {
+                isNew = "";
+                break;
+            }
+        }
+        $("#" + id).append("<tr><td>" + values.cur[value][0] + "</td><td>" + values.cur[value][1] + "</td><td>" + isNew + "</td></tr>");
+    }
+}
+
+function updateTable2(values, id) {
+    for (value in values.cur) {
+        var isNew = "New";
+        for (value2 in values.prev) {
+            if (values.cur[value][0] === values.prev[value2][0] && values.cur[value][1] === values.prev[value2][1] && values.cur[value][2] === values.prev[value2][2]) {
+                isNew = "";
+                break;
+            }
+        }
+        $("#" + id).append("<tr><td>" + values.cur[value][0] + " and " + values.cur[value][1] + "</td><td>" + values.cur[value][2] + "</td><td>" + isNew + "</td></tr>");
+    }
+}
+
+function scroll() {
+    var height = $('#navbar').height();
+    if ($(this).scrollTop() + height + 10 > $("#charts").position().top) {
+        $('#upgrades').css({
+            "position": "fixed",
+            "top": height + 10,
+            "right": 0});
+    } else {
+        $('#upgrades').css({
+            "position": "relative",
+            "top": "auto",
+            "right": "auto"});
+    }
+}
+
+function tooltip(id, title, show) {
+    show = (typeof show === 'undefined' ? true : show);
+    $("#" + id).attr("title", title).tooltip('fixTitle');
+    if (show) {
+        $("#" + id).tooltip('show');
+    }
+}
+
+function centerModal() {
+    $(this).show();
+    var $dialog = $(this).find(".modal-dialog").css("margin-top", offset);;
+    var offset = ($(window).height() - $dialog.height()) / 2;
+    $dialog.css("margin-top", offset);
+}
+
+function format(nStr) {
+    nStr += '';
+    x = nStr.split('.');
+    x1 = x[0];
+    x2 = x.length > 1 ? '.' + x[1] : '';
+    var rgx = /(\d+)(\d{3})/;
+    while (rgx.test(x1)) {
+        x1 = x1.replace(rgx, '$1' + ',' + '$2');
+    }
+    return x1 + x2;
+}
+
+$(window).scroll(scroll);
+
+/*
+(function($) {
+    $.eventReport = function(selector, root) {
+        var s = [];
+        $(selector || '*', root).addBack().each(function() {
+            // the following line is the only change
+            var e = $._data(this, 'events');
+            if(!e) return;
+            s.push(this.tagName);
+            if(this.id) s.push('#', this.id);
+            if(this.className) s.push('.', this.className.replace(/ +/g, '.'));
+            for(var p in e) {
+                var r = e[p],
+                    h = r.length - r.delegateCount;
+                if(h)
+                    s.push('\n', h, ' ', p, ' handler', h > 1 ? 's' : '');
+                if(r.delegateCount) {
+                    for(var q = 0; q < r.length; q++)
+                        if(r[q].selector) s.push('\n', p, ' for ', r[q].selector);
+                }
+            }
+            s.push('\n\n');
+        });
+        return s.join('');
+    }
+    $.fn.eventReport = function(selector) {
+        return $.eventReport(selector, this);
+    }
+})(jQuery);
+*/
