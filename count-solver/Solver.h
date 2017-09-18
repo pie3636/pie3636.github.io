3f2e85408dedc08f821ab2c43270bb196806dc0c
Maxime Méloux <maxime.meloux@ensimag.grenoble-inp.fr>
2017-09-18 16:33:50 +0200

    Improved heuristic, added debug traces

--- count-solver/Solver.h
+++ count-solver/Solver.h
@@ -10,6 +10,7 @@
 #include <vector>
 #include <algorithm>
 #include <cmath>
+#include <map>
 
 class Solver {
     private:
@@ -17,18 +18,14 @@ class Solver {
         const std::vector <unsigned> base = {288, 210, 24, 20, 19, 18, 17, 16, 15, 14, 13, 12, 11, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1};
 
         const UnaryOperator *const unaryOperators[1] = {
-                new AbsoluteValue(1)
+                new AbsoluteValue(COST_ABS)
         };
         const BinaryOperator *const binaryOperators[4] = {
-                new Plus(1),
-                new Minus(1),
-                new Times(1),
-                new Divide(1)
+                new Plus(COST_PLUS),
+                new Minus(COST_MINUS),
+                new Times(COST_TIMES),
+                new Divide(COST_DIVIDE)
         };
-
-        std::string invertPrime(unsigned i, int fours) const {
-            return "P(" + heuristic4444((uint64_t) std::distance(primes.begin(), std::find(primes.begin(), primes.end(), i)) + 1, fours) + ")";
-        }
 
         int largestPrimeDiv(int i) const {
             for (int j : primes) {
@@ -46,7 +43,7 @@ class Solver {
             return heuristic.find('<') == std::string::npos && heuristic.find('>') == std::string::npos;
         }
 
-        unsigned ipow(unsigned a, unsigned b)const {
+        unsigned ipow(unsigned a, unsigned b) const {
             unsigned res = 1;
             while (b) {
                 if (b & 1) {
@@ -56,6 +53,10 @@ class Solver {
                 a *= a;
             }
             return res;
+        }
+
+        std::string invertPrime(unsigned i, int fours) const {
+            return "P(" + heuristic4444(baseInvertPrime(i), fours) + ")";
         }
 
     public:
@@ -83,9 +84,10 @@ class Solver {
             return *this;
         }
 
-        std::string heuristic4444(uint64_t i, int fours = 4) const {
-            std::string heur, heur2, heur3;
-            unsigned div;
+        std::string heuristic4444(uint64_t i, int fours = 4, bool trace = false, int depth = 0, int line = 0) const {
+            if (trace) std::cout << "[DEBUG] Entering h4 <" << fours << '\t' << i << '\t' << depth << "> from " << line << std::endl;
+            std::string heur, heur2, heur3, savedHeur3 = "";
+            unsigned div, divCoeff, r, copy;
             if (i == 2) {
                 return fours == 1 ? "Ω(4)" :
                        fours == 2 ? "(4 / Ω(4))" :
@@ -166,9 +168,18 @@ class Solver {
                     if (i == 256) {
                         return "4^4";
                     }
-                    for (unsigned j = 3; j * j * j * j < MAX; j++) {
-                        if (i == j * j * j * j) {
-                            return "(" + heuristic4444(j, 1) + ") ^ 4";
+                    r = (unsigned) sqrt(sqrt(i));
+                    if (i == r * r * r * r) {
+                        heur = heuristic4444(r, 1, trace, depth + 1, __LINE__);
+                        if (isValid(heur)) {
+                            return "(" + heur + ") ^ 4";
+                        }
+                    }
+                    r = (unsigned) sqrt(i);
+                    if (i == r * r) {
+                        heur = heuristic4444(r, 1, trace, depth + 1, __LINE__);
+                        if (isValid(heur)) {
+                            return "(" + heur + ") ^ Ω(4)";
                         }
                     }
                 case 3:
@@ -177,7 +188,7 @@ class Solver {
                         return invertPrime(i, fours);
                     }
                     if (fours != 2) {
-                        heur = heuristic4444(i, 2);
+                        heur = heuristic4444(i, 2, trace, depth + 1, __LINE__);
                         if (isValid(heur)) {
                             return fours == 4 ?
                                    heur + " + 4 - 4" :
@@ -185,9 +196,9 @@ class Solver {
                         }
                     }
                     if (fours == 4) {
-                        heur = heuristic4444(i, 3);
+                        heur = heuristic4444(i, 3, trace, depth + 1, __LINE__);
                         if (isValid(heur)) {
-                            return heur + " × ω(4)";
+                            savedHeur3 = heur;
                         }
                     }
                     break;
@@ -195,76 +206,83 @@ class Solver {
                     return "<02-" + std::to_string(i) + "@" + std::to_string(fours) + ">";
             }
             div = (unsigned int) largestPrimeDiv((int) i);
+            divCoeff = 1;
+            copy = i /div;
+            while (copy % div == 0) {
+                copy /= div;
+                divCoeff++;
+            }
+
             unsigned val, valMax;
-            switch(fours) {
+            switch (fours) {
                 case 2:
-                    heur = heuristic4444(div, 1);
-                    heur2 = heuristic4444(i/div, 1);
+                    heur = heuristic4444(div, 1, trace, depth + 1, __LINE__);
+                    heur2 = heuristic4444(i/div, 1, trace, depth + 1, __LINE__);
                     if (isValid(heur) && isValid(heur2)) {
                         return heur + " × " + heur2;
                     }
                     break;
                 case 3:
-                    for (unsigned k = (unsigned)(floor(log(MAX) / log(2))); k >= 3; k--) { // Testing n**k
+                    for (unsigned k = (unsigned)(floor(log(MAX) / log(2))); k >= 2; k--) { // Testing n**k
                         for (valMax = 2; ipow(valMax, k) <= MAX; valMax++);
                         for (unsigned j = valMax; j >= 2; j--) {
                             val = ipow(j, k);
                             if (i == val) {
                                 for (int l = 1; l <= 2; l++) {
-                                    heur = heuristic4444(j, l);
-                                    heur2 = heuristic4444(k, 3 - l);
+                                    heur = heuristic4444(j, l, trace, depth + 1, __LINE__);
+                                    heur2 = heuristic4444(k, 3 - l, trace, depth + 1, __LINE__);
                                     if (isValid(heur) && isValid(heur2)) {
-                                        return heur + " ^ " + heur2;
+                                        return "(" + heur + ") ^ " + heur2;
                                     }
                                 }
                             }
                             if (i % val == 0) {
                                 heur = heuristic4444(i / val, 1);
-                                heur2 = heuristic4444(j, 1);
-                                heur3 = heuristic4444(k, 1);
+                                heur2 = heuristic4444(j, 1, trace, depth + 1, __LINE__);
+                                heur3 = heuristic4444(k, 1, trace, depth + 1, __LINE__);
                                 if (isValid(heur) && isValid(heur2) && isValid(heur3)) {
-                                    return heur + " × " + heur2 + " ^ " + heur3;
+                                    return heur + " × (" + heur2 + ") ^ " + heur3;
                                 }
                             }
                         }
                     }
                     for (int l = 1; l <= 2; l++) {
-                        heur = heuristic4444(div, l);
-                        heur2 = heuristic4444(i / div, 3 - l);
+                        heur = heuristic4444(div, l, trace, depth + 1, __LINE__);
+                        heur2 = heuristic4444(i / div, 3 - l, trace, depth + 1, __LINE__);
                         if (isValid(heur) && isValid(heur2)) {
                             return heur + " × " + heur2;
                         }
                     }
                     break;
                 case 4:
-                    for (unsigned k = (unsigned)(floor(log(MAX) / log(2))); k >= 3; k--) { // Testing n**k
+                    for (unsigned k = (unsigned)(floor(log(MAX) / log(2))); k >= 2; k--) { // Testing n**k
                         for (valMax = 2; ipow(valMax, k) <= MAX; valMax++);
                         for (unsigned j = valMax; j >= 2; j--) {
                             val = ipow(j, k);
                             if (i == val) {
                                 for (int l = 1; l <= 3; l++) {
-                                    heur = heuristic4444(j, l);
-                                    heur2 = heuristic4444(k, 4 - l);
+                                    heur = heuristic4444(j, l, trace, depth + 1, __LINE__);
+                                    heur2 = heuristic4444(k, 4 - l, trace, depth + 1, __LINE__);
                                     if (isValid(heur) && isValid(heur2)) {
-                                        return heur + " ^ " + heur2;
+                                        return "(" + heur + ") ^ " + heur2;
                                     }
                                 }
                             }
                             if (i % val == 0) {
                                 for (int l = 1; l <= 3; l++) {
-                                    heur = heuristic4444(i / val, 1 + (l == 1 ? 1 : 0));
-                                    heur2 = heuristic4444(j, 1 + (l == 2 ? 1 : 0));
-                                    heur3 = heuristic4444(k, 1 + (l == 3 ? 1 : 0));
+                                    heur = heuristic4444(i / val, 1 + (l == 1 ? 1 : 0), trace, depth + 1, __LINE__);
+                                    heur2 = heuristic4444(j, 1 + (l == 2 ? 1 : 0), trace, depth + 1, __LINE__);
+                                    heur3 = heuristic4444(k, 1 + (l == 3 ? 1 : 0), trace, depth + 1, __LINE__);
                                     if (isValid(heur) && isValid(heur2) && isValid(heur3)) {
-                                        return heur + " × " + heur2 + " ^ " + heur3;
+                                        return heur + " × (" + heur2 + ") ^ " + heur3;
                                     }
                                 }
                             }
                         }
                     }
                     for (int l = 1; l <= 3; l++) {
-                        heur = heuristic4444(div, l);
-                        heur2 = heuristic4444(i / div, 4 - l);
+                        heur = heuristic4444(div, l, trace, depth + 1, __LINE__);
+                        heur2 = heuristic4444(i / div, 4 - l, trace, depth + 1, __LINE__);
                         if (isValid(heur) && isValid(heur2)) {
                             return heur + " × " + heur2;
                         }
@@ -274,31 +292,31 @@ class Solver {
                     return "<03-" + std::to_string(i) + "@" + std::to_string(fours) + ">";
             }
             if (i % 24 == 0) {
-                heur = heuristic4444(i / 24, fours - 1);
+                heur = heuristic4444(i / 24, fours - 1, trace, depth + 1, __LINE__);
                 if (isValid(heur)) {
                     return "4! × " + heur;
                 }
             }
             if (i % 8 == 0) {
-                heur = heuristic4444(i / 8, fours - 1);
+                heur = heuristic4444(i / 8, fours - 1, trace, depth + 1, __LINE__);
                 if (isValid(heur)) {
                     return "4!! × " + heur;
                 }
             }
             if (i % 4 == 0) {
-                heur = heuristic4444(i / 4, fours - 1);
+                heur = heuristic4444(i / 4, fours - 1, trace, depth + 1, __LINE__);
                 if (isValid(heur)) {
                     return "4 × " + heur;
                 }
             }
             if (fours >= 2 && i % 6 == 0) {
-                heur = heuristic4444(i / 6, fours - 1);
+                heur = heuristic4444(i / 6, fours - 1, trace, depth + 1, __LINE__);
                 if (isValid(heur)) {
                     return "Γ(4) × " + heur;
                 }
             }
             if (fours >= 2 && i % 2 == 0) {
-                heur = heuristic4444(i / 2, fours - 1);
+                heur = heuristic4444(i / 2, fours - 1, trace, depth + 1, __LINE__);
                 if (isValid(heur)) {
                     return "Ω(4) × " + heur;
                 }
@@ -306,21 +324,21 @@ class Solver {
             for (unsigned j = (unsigned) sqrt(i); j >= 2; j--) {
                 val = j * j;
                 if (i == val && fours >= 2) {
-                    heur = heuristic4444(j, fours - 1);
+                    heur = heuristic4444(j, fours - 1, trace, depth + 1, __LINE__);
                     if (isValid(heur)) {
                         return heur + " ^ Ω(4)";
                     }
                 }
                 if (i % val == 0) {
                     for (int l = 1; l < fours - 1; l++) {
-                        heur = heuristic4444(i / val, l);
-                        heur2 = heuristic4444(j, fours - 1 - l);
+                        heur = heuristic4444(i / val, l, trace, depth + 1, __LINE__);
+                        heur2 = heuristic4444(j, fours - 1 - l, trace, depth + 1, __LINE__);
                         if (isValid(heur) && isValid(heur2)) {
                             return "(" + heur + " × " + heur2 + ") ^ Ω(4)";
                         }
                     }
                     for (int l = 1; l < fours - 1; l++) {
-                        heur = heuristic4444(i / val * j, fours - 1);
+                        heur = heuristic4444(i / val * j, fours - 1, trace, depth + 1, __LINE__);
                         if (isValid(heur)) {
                             return heur + " ^ Ω(4)";
                         }
@@ -329,14 +347,72 @@ class Solver {
             }
             for (unsigned j : base) {
                 if (i % j == 0) {
-                    heur = heuristic4444(i / j, fours - 1);
-                    heur2 = heuristic4444(j, 1);
+                    heur = heuristic4444(i / j, fours - 1, trace, depth + 1, __LINE__);
+                    heur2 = heuristic4444(j, 1, trace, depth + 1, __LINE__);
                     if (isValid(heur)) {
                         return heur + " × " + heur2;
                     }
                 }
             }
+            if (savedHeur3 != "") {
+                return savedHeur3 + " × ω(4)";
+            }
             return "<4-" + std::to_string(i) + "@" + std::to_string(fours) + ">";
+        }
+
+        uint64_t evaluateCost(std::string solve) const {
+            std::vector <std::pair <std::string, int>> costs = {
+                    {"!!", COST_DOUBLE_FACTORIAL},
+                    {"sf", COST_SUPERFACTORIAL},
+                    {" ", 0},
+                    {"0", COST_DIGIT},
+                    {"1", COST_DIGIT},
+                    {"2", COST_DIGIT},
+                    {"3", COST_DIGIT},
+                    {"4", COST_DIGIT},
+                    {"5", COST_DIGIT},
+                    {"6", COST_DIGIT},
+                    {"7", COST_DIGIT},
+                    {"8", COST_DIGIT},
+                    {"9", COST_DIGIT},
+                    {"p#", COST_PRIMORIAL},
+                    // End multi-character sequences
+                    {"(", COST_LPAREN},
+                    {")", COST_RPAREN},
+                    {"!", COST_FACTORIAL},
+                    {"+", COST_PLUS},
+                    {"-", COST_MINUS},
+                    {"×", COST_TIMES},
+                    {"/", COST_DIVIDE},
+                    {"^", COST_POWER},
+                    {"Γ", COST_GAMMA},
+                    {"p", COST_PARTITION},
+                    {"σ", COST_SIGMA},
+                    {"d", COST_DIVISOR_COUNT},
+                    {"C", COST_CATALAN},
+                    {"ω", COST_SMALL_OMEGA},
+                    {"Ω", COST_BIG_OMEGA},
+                    {"P", COST_PRIME_NUMBER},
+                    {"F", COST_FIBONACCI},
+                    {"s", COST_DIVISOR_SUM}
+            };
+            int cost = 0;
+            for (auto i : costs) {
+                auto pos = solve.find(i.first);
+                while (pos != std::string::npos) {
+                    solve.erase(pos, i.first.length());
+                    pos = solve.find(i.first);
+                    cost += i.second;
+                }
+            }
+            if (solve.length() > 0) {
+                std::cout << "Missing costs for <" << solve << ">" << std::endl;
+            }
+            return cost;
+        }
+
+        uint64_t baseInvertPrime(unsigned i) const {
+            return (uint64_t) std::distance(primes.begin(), std::find(primes.begin(), primes.end(), i)) + 1;
         }
 };
 
--- count-solver/main.cpp
+++ count-solver/main.cpp
@@ -1,6 +1,74 @@
 #define MAX 10000
 
-// #define DEBUG
+#define COST_DIGIT 0
+#define COST_PLUS 1
+#define COST_MINUS 1
+#define COST_TIMES 1
+#define COST_DIVIDE 1
+#define COST_ABS 1
+//
+#define COST_DOUBLE_FACTORIAL 2
+#define COST_FACTORIAL 2
+#define COST_SUPERFACTORIAL 2
+#define COST_LPAREN 0
+#define COST_RPAREN 0
+#define COST_POWER 1
+#define COST_GAMMA 2
+#define COST_PARTITION 3
+#define COST_SIGMA 2
+#define COST_DIVISOR_COUNT 2
+#define COST_CATALAN 2
+#define COST_SMALL_OMEGA 2
+#define COST_BIG_OMEGA 2
+#define COST_PRIME_NUMBER 2
+#define COST_FIBONACCI 2
+#define COST_DIVISOR_SUM 2
+#define COST_PRIMORIAL 3
+
+/*
+    MODULO: 1,
+    CONCAT: 1,
+    KNUTH_ARROW: 2,
+    HYPERGEOM: 2,
+    SQUARE_ROOT: 2,
+    TRIBONACCI: 2,
+    PRIME_COUNT: 2,
+    MOBIUS: 2,
+    EULER_TOTIENT: 2,
+    MULTISIGMADIGIT: 3,
+    ACKERMANN: 3,
+    SIGN: 4,
+    EXPONENTIAL: 4,
+    LOGARITHM: 4,
+    DECIMAL_SEPARATOR: 4,
+    REPEATING_DECIMAL: 4,
+    PERCENT: 4,
+    SINE: 5,
+    COSINE: 5,
+    TANGENT: 5,
+    SECANT: 5,
+    COSECANT: 5,
+    COTANGENT: 5,
+    ARC_SINE: 5,
+    ARC_COSINE: 5,
+    ARC_TANGENT: 5,
+    ARC_SECANT: 5,
+    ARC_COSECANT: 5,
+    ARC_COTANGENT: 5,
+    FLOOR: 6,
+    CEIL: 6,
+    BACKWARDS: 6,
+    BINARYBEANSTALK: 7,
+    NONPRIME: 7,
+    TOTIENT_SUM: 7
+*/
+
+/*
+ * RELEASE
+ * DEBUG1 (test min)
+ * DEBUG2 (test all)
+ */
+#define DEBUG1
 
 #include <iostream>
 #include "Solver.h"
@@ -9,8 +77,8 @@ int main() {
     system("chcp 65001");
     Solver solver;
     std::string res;
-#ifndef DEBUG
-    unsigned i;
+
+#ifdef RELEASE
     std::cout << "4-4-4-4 heuristical solver v. 0.1795.1740 (tested up to 10,000)" << std::endl;
     std::cout << "Message /u/pie3636 if you find any incorrect values" << std::endl;
     std::cout << std::endl;
@@ -18,75 +86,52 @@ int main() {
     std::cout << "In addition, this solution is most likely not optimal." << std::endl;
     std::cout << std::endl;
     std::cout << "Number to solve ? ";
+    unsigned i;
     std::cin >> i;
     std::cout << std::endl;
     res = solver.heuristic4444(i);
-    if (res.find('<') == std::string::npos || std::count(res.begin(), res.end(), '4' != 4)) {
-        std::cout << solver.heuristic4444(i) << " = " << i << std::endl;
-    } else {
+    if (res.find('<') != std::string::npos && std::count(res.begin(), res.end(), '4' == 4)) {
         std::cout << "The solver was unable to solve this value." << std::endl;
+        return EXIT_FAILURE;
     }
-#else
-    for (int i = 1; i < MAX; i++) {
+    std::cout << solver.heuristic4444(i) << " = " << i << std::endl;
+    uint64_t cost = solver.evaluateCost(res);
+    std::cout << "Cost: " << cost << std::endl;
+#endif
+#ifdef DEBUG1
+    uint64_t minCost = 1000, maxCost = 0, sumCost = 0, indexMin, indexMax;
+    for (uint64_t i = 1; i < MAX; i++) {
         res = solver.heuristic4444(i);
         if (res.find('<') != std::string::npos || std::count(res.begin(), res.end(), '4' != 4)) {
             std::cout << solver.heuristic4444(i) << ": " << i << std::endl;
         }
+        uint64_t cost = solver.evaluateCost(res);
+        if (cost < minCost) {
+            minCost = cost;
+            indexMin = i;
+        }
+        if (cost > maxCost) {
+            maxCost = cost;
+            indexMax = i;
+        }
+        sumCost += cost;
     }
+    std::cout << std::endl;
+    std::cout << "Min of " << minCost << " at " << indexMin << std::endl;
+    std::cout << "Min of " << maxCost << " at " << indexMax << std::endl;
+    std::cout << "Avg of " << (double)sumCost/MAX << std::endl;
+    std::cout << solver.heuristic4444(indexMax) << std::endl;
+#endif
+#ifdef DEBUG2
+    unsigned i = 4761;
+    res = solver.heuristic4444(i);
+    if (res.find('<') != std::string::npos && std::count(res.begin(), res.end(), '4' == 4)) {
+        std::cout << "The solver was unable to solve this value." << std::endl;
+        return EXIT_FAILURE;
+    }
+    std::cout << solver.heuristic4444(i, 4, true) << " = " << i << std::endl;
+    uint64_t cost = solver.evaluateCost(res);
+    std::cout << "Cost: " << cost << std::endl;
 #endif
     system("pause");
-    /*
-        DIGIT: 0,
-        PARENTHESIS: 0,
-        POWER: 1,
-        MODULO: 1,
-        CONCAT: 1,
-        KNUTH_ARROW: 2,
-        FACTORIAL: 2,
-        DOUBLE_FACTORIAL: 2,
-        SUPERFACTORIAL: 2,
-        HYPERGEOM: 2,
-        GAMMA: 2,
-        SQUARE_ROOT: 2,
-        PARTITION: 2,
-        MOBIUS: 2,
-        FIBONACCI: 2,
-        TRIBONACCI: 2,
-        SIGMA: 2,
-        DIVISOR_SUM: 2,
-        DIVISOR_COUNT: 2,
-        CATALAN: 2,
-        SMALL_OMEGA: 2,
-        BIG_OMEGA: 2,
-        PRIME_COUNT: 2,
-        PRIME_NUMBER: 2,
-        EULER_TOTIENT: 2,
-        MULTISIGMADIGIT: 3,
-        ACKERMANN: 3,
-        PRIMORIAL: 3,
-        SIGN: 4,
-        EXPONENTIAL: 4,
-        LOGARITHM: 4,
-        DECIMAL_SEPARATOR: 4,
-        REPEATING_DECIMAL: 4,
-        PERCENT: 4,
-        SINE: 5,
-        COSINE: 5,
-        TANGENT: 5,
-        SECANT: 5,
-        COSECANT: 5,
-        COTANGENT: 5,
-        ARC_SINE: 5,
-        ARC_COSINE: 5,
-        ARC_TANGENT: 5,
-        ARC_SECANT: 5,
-        ARC_COSECANT: 5,
-        ARC_COTANGENT: 5,
-        FLOOR: 6,
-        CEIL: 6,
-        BACKWARDS: 6,
-        BINARYBEANSTALK: 7,
-        NONPRIME: 7,
-        TOTIENT_SUM: 7
-    */
 }
