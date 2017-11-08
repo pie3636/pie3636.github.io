a1f65db0d2a33bbc6b9d362e2486724c67edada2
Maxime Méloux <maxime.meloux@ensimag.grenoble-inp.fr>
2017-11-08 17:28:42 +0100

    Fixed instanceof

--- tug-of-war/main.js
+++ tug-of-war/main.js
@@ -7,146 +7,6 @@ function changeTab(newTab) {
         scroll();
 }
 
-$(function () {
-        $("#loading").remove();
-        currentTab = "charts";
-        changeTab("charts");
-        
-        $("#version").append(data.lastData);
-        $("#version-mobile").append(data.lastData.substr(0, 7));
-        $("#updateAnnouncementVersion").html(data.lastUpdate);
-
-        $('.modal').on('show.bs.modal', centerModal);
-        $(window).on("resize", function() {
-            $('.modal:visible').each(centerModal);
-        });
-        
-        var storedVersion = localStorage.storedVersion;
-        if (typeof storedVersion === "undefined" || storedVersion !== data.lastUpdate) {
-            $("#updateAnnouncement").show();
-        }
-        $("#updateAnnouncementClose").click(function() {
-            $("#updateAnnouncement").hide();
-            localStorage.storedVersion = data.lastUpdate;
-        });
-        
-        /******************** Data inclusion ********************/
-        
-        if (data.comment !== "") {
-            $("#comment").html(data.comment.replace(/\n/g, "<br />"));
-            $("#comment-panel").show();
-        }
-        
-        for (var i in data.charts) {
-            $("#charts-link-" + i).attr("href", data.charts[i]);
-            $("#charts-img-" + i).attr("src", data.charts[i]);
-        }
-        
-        updateValue(data.counts.total, "total-counts", true);
-        updateValue(data.counts.ofMain, "of-main-counts-percent", true);
-        updateValue(data.counts.min, "min-count", true);
-        updateValue(data.counts.max, "max-count", true);
-        updateValue(data.counts.range, "count-range", true);
-        updateValue(data.counts.ofMaxRange, "of-max-count-range", true, 2);
-        updateValue(data.counts.avg, "avg-count", true, 2);
-        updateValue(data.counts.med, "med-count", true);
-        
-        updateTableNR(data.counts.mostCommon, "most-common-counts", false, true);
-        updateTableNR(data.counts.leastCommon, "least-common-counts", false, true);
-        
-        updateValue(data.replyTime.started, "started", true, 2, true);
-        updateValue(data.replyTime.fastest, "fastest-count", true, 2, true);
-        updateValue(data.replyTime.slowest, "slowest-count", true, 2, true);
-        updateValue(data.replyTime.avg, "avg-time", true, 2, true);
-        updateValue(data.replyTime.med, "med-time", true, 0, true);
-        updateValue(data.countSign.pos, "pos-counts", true);
-        updateValue(data.countSign.neg, "neg-counts", true);
-        updateValue(data.countSign.posPercent, "pos-counts-percent", true, 2);
-        updateValue(data.countSign.negPercent, "neg-counts-percent", true, 2);
-        updateValue(data.countSign.zeros, "zero-counts", true);
-        updateValue(data.countSign.ofZero, "of-counts-zero", true, 2);
-        updateValue(data.countSign.maxPosStreak, "max-pos-streak", true);
-        updateValue(data.countSign.maxNegStreak, "max-neg-streak", true);
-        updateValue(data.countSign.maxUpStreak.length, "max-up-streak", true);
-        updateValue(data.countSign.maxUpStreak.percent, "max-up-streak-percent", true, 2);
-        updateValue(data.countSign.maxUpStreak.begin, "max-up-streak-begin");
-        updateValue(data.countSign.maxUpStreak.end, "max-up-streak-end");
-        updateValue(data.countSign.maxDownStreak.length, "max-down-streak", true);
-        updateValue(data.countSign.maxDownStreak.percent, "max-down-streak-percent", true, 2);
-        updateValue(data.countSign.maxDownStreak.begin, "max-down-streak-begin");
-        updateValue(data.countSign.maxDownStreak.end, "max-down-streak-end");
-        updateValue(data.countSign.avgStreak, "avg-streak", true);
-        updateValue(data.countSign.posNegAsym, "pos-neg-asym", true);
-        updateValue(data.gets.count, "total-gets", true);
-        updateValue(data.gets.avg, "avg-get", true, 2);
-        updateValue(data.gets.med, "med-get", true);
-        
-        updateTableNR(data.gets.mostCommon, "most-common-gets", false, true);
-        updateTableNR(data.gets.leastCommon, "least-common-gets", false, true);
-        
-        updateValue(data.gets.fastest.time, "fastest-get", true, 2, true);
-        $("#fastest-get-direction").html(data.gets.fastest.begin < data.gets.fastest.end ? "up" : "down")
-        updateValue(data.gets.fastest.begin, "fastest-get-begin");
-        updateValue(data.gets.fastest.end, "fastest-get-end");
-        updateValue(data.gets.slowest.time, "slowest-get", true, 2, true);
-        $("#slowest-get-direction").html(data.gets.slowest.begin < data.gets.slowest.end ? "up" : "down")
-        updateValue(data.gets.slowest.begin, "slowest-get-begin");
-        updateValue(data.gets.slowest.end, "slowest-get-end");
-        updateValue(data.gets.avgTime, "avg-get-time", true, 2, true);
-        updateValue(data.gets.medTime, "med-get-time", true, 2, true);
-        updateValue(data.gets.longest.length, "longest-get", true, 2, true);
-        $("#longest-get-direction").html(data.gets.longest.begin < data.gets.longest.end ? "up" : "down")
-        updateValue(data.gets.longest.begin, "longest-get-begin");
-        updateValue(data.gets.longest.end, "longest-get-end");
-        updateValue(data.gets.avgLen, "avg-get-len", true, 2);
-        updateValue(data.gets.medLen, "med-get-len", true);
-        updateValue(data.gets.lastLen, "last-get-len");
-        updateValue(data.getSign.maxUpStreak.length, "max-up-get-streak", true);
-        updateValue(data.getSign.maxUpStreak.begin, "max-up-get-streak-begin");
-        updateValue(data.getSign.maxUpStreak.end, "max-up-get-streak-end");
-        updateValue(data.getSign.maxDownStreak.length, "max-down-get-streak", true);
-        updateValue(data.getSign.maxDownStreak.begin, "max-down-get-streak-begin");
-        updateValue(data.getSign.maxDownStreak.end, "max-down-get-streak-end");
-        updateValue(data.getSign.avgStreak, "avg-get-streak", true);
-        updateValue(data.getSign.posNegAsym, "pos-neg-get-asym", true);
-        updateValue(data.getSign.pos, "pos-gets", true);
-        updateValue(data.getSign.neg, "neg-gets", true);
-        updateValue(data.getSign.posPercent, "pos-gets-percent", true, 2);
-        updateValue(data.getSign.negPercent, "neg-gets-percent", true, 2);
-        updateValue(data.getSign.zeros, "zero-gets", true);
-        updateValue(data.getSign.ofZero, "of-gets-zero", true, 2);
-        updateValue(data.getSign.maxPosStreak, "max-pos-get-streak", true);
-        updateValue(data.getSign.maxNegStreak, "max-neg-get-streak", true);
-        
-        updateTableNR(data.oneStalemates, "one-stalemates");
-        updateTableNR(data.twoStalemates, "two-stalemates", true);
-        
-        updateValue(data.deletedCounts, "deleted-counts", true);
-        updateValue(data.forks, "forks", true);
-        updateValue(data.users.count, "total-usrs", true);
-        updateValue(data.users.avg, "avg-usr", true, 2);
-        updateValue(data.users.med, "med-usr", true);
-        
-        updateTableR(data.users.top20, "top-20-usrs");
-        updateTableR(data.users.topGets, "top-usr-gets");
-        
-        updateValue(data.users.topGets.threshold, "usr-min-gets");
-        
-        updateTableR(data.users.topAssists, "top-usr-assists");
-        
-        updateValue(data.users.topAssists.threshold, "usr-min-assists");
-        
-        updateTableR(data.users.fastest, "top-usr-speed");
-        
-        updateValue(data.users.fastest.threshold, "usr-min-counts");
-        
-        updateTableR(data.users.fastestMed, "top-usr-med-speed");
-        updateTableR(data.users.speedScore, "top-usr-speed-score");
-        updateTableR(data.users.topTeamScore, "top-team-score-_and");
-        
-        updateTableR(data.top100, "top-100-usrs");
-});
-
 function updateValue(value, id, hasVariation, digits, hasUnit) {
     // Try to read "digits" from parameter, if it fails try to read from
     hasDigits = typeof digits !== "undefined" && digits !== 0 || typeof (digits = value.precision) !== "undefined"; JSON
@@ -282,8 +142,10 @@ function deltaFor(curVal, map, col) {
             return " <b>(" + str + ")</b>";
         }
         return str;
-    } catch (e if e instanceof TypeError) {
-        return "";
+    } catch (e) {
+        if (e instanceof TypeError) {
+            return "";
+        }
     }
 }
 
@@ -366,3 +228,144 @@ function format(nStr) {
     }
     return x1 + x2;
 }
+
+$(document).ready(function() {
+    console.log("nojn");
+    $("#loading").remove();
+    currentTab = "charts";
+    changeTab("charts");
+    
+    $("#version").append(data.lastData);
+    $("#version-mobile").append(data.lastData.substr(0, 7));
+    $("#updateAnnouncementVersion").html(data.lastUpdate);
+
+    $('.modal').on('show.bs.modal', centerModal);
+    $(window).on("resize", function() {
+        $('.modal:visible').each(centerModal);
+    });
+    
+    var storedVersion = localStorage.storedVersion;
+    if (typeof storedVersion === "undefined" || storedVersion !== data.lastUpdate) {
+        $("#updateAnnouncement").show();
+    }
+    $("#updateAnnouncementClose").click(function() {
+        $("#updateAnnouncement").hide();
+        localStorage.storedVersion = data.lastUpdate;
+    });
+    
+    /******************** Data inclusion ********************/
+    
+    if (data.comment !== "") {
+        $("#comment").html(data.comment.replace(/\n/g, "<br />"));
+        $("#comment-panel").show();
+    }
+    
+    for (var i in data.charts) {
+        $("#charts-link-" + i).attr("href", data.charts[i]);
+        $("#charts-img-" + i).attr("src", data.charts[i]);
+    }
+    
+    updateValue(data.counts.total, "total-counts", true);
+    updateValue(data.counts.ofMain, "of-main-counts-percent", true);
+    updateValue(data.counts.min, "min-count", true);
+    updateValue(data.counts.max, "max-count", true);
+    updateValue(data.counts.range, "count-range", true);
+    updateValue(data.counts.ofMaxRange, "of-max-count-range", true, 2);
+    updateValue(data.counts.avg, "avg-count", true, 2);
+    updateValue(data.counts.med, "med-count", true);
+    
+    updateTableNR(data.counts.mostCommon, "most-common-counts", false, true);
+    updateTableNR(data.counts.leastCommon, "least-common-counts", false, true);
+    
+    updateValue(data.replyTime.started, "started", true, 2, true);
+    updateValue(data.replyTime.fastest, "fastest-count", true, 2, true);
+    updateValue(data.replyTime.slowest, "slowest-count", true, 2, true);
+    updateValue(data.replyTime.avg, "avg-time", true, 2, true);
+    updateValue(data.replyTime.med, "med-time", true, 0, true);
+    updateValue(data.countSign.pos, "pos-counts", true);
+    updateValue(data.countSign.neg, "neg-counts", true);
+    updateValue(data.countSign.posPercent, "pos-counts-percent", true, 2);
+    updateValue(data.countSign.negPercent, "neg-counts-percent", true, 2);
+    updateValue(data.countSign.zeros, "zero-counts", true);
+    updateValue(data.countSign.ofZero, "of-counts-zero", true, 2);
+    updateValue(data.countSign.maxPosStreak, "max-pos-streak", true);
+    updateValue(data.countSign.maxNegStreak, "max-neg-streak", true);
+    updateValue(data.countSign.maxUpStreak.length, "max-up-streak", true);
+    updateValue(data.countSign.maxUpStreak.percent, "max-up-streak-percent", true, 2);
+    updateValue(data.countSign.maxUpStreak.begin, "max-up-streak-begin");
+    updateValue(data.countSign.maxUpStreak.end, "max-up-streak-end");
+    updateValue(data.countSign.maxDownStreak.length, "max-down-streak", true);
+    updateValue(data.countSign.maxDownStreak.percent, "max-down-streak-percent", true, 2);
+    updateValue(data.countSign.maxDownStreak.begin, "max-down-streak-begin");
+    updateValue(data.countSign.maxDownStreak.end, "max-down-streak-end");
+    updateValue(data.countSign.avgStreak, "avg-streak", true);
+    updateValue(data.countSign.posNegAsym, "pos-neg-asym", true);
+    updateValue(data.gets.count, "total-gets", true);
+    updateValue(data.gets.avg, "avg-get", true, 2);
+    updateValue(data.gets.med, "med-get", true);
+    
+    updateTableNR(data.gets.mostCommon, "most-common-gets", false, true);
+    updateTableNR(data.gets.leastCommon, "least-common-gets", false, true);
+    
+    updateValue(data.gets.fastest.time, "fastest-get", true, 2, true);
+    $("#fastest-get-direction").html(data.gets.fastest.begin < data.gets.fastest.end ? "up" : "down")
+    updateValue(data.gets.fastest.begin, "fastest-get-begin");
+    updateValue(data.gets.fastest.end, "fastest-get-end");
+    updateValue(data.gets.slowest.time, "slowest-get", true, 2, true);
+    $("#slowest-get-direction").html(data.gets.slowest.begin < data.gets.slowest.end ? "up" : "down")
+    updateValue(data.gets.slowest.begin, "slowest-get-begin");
+    updateValue(data.gets.slowest.end, "slowest-get-end");
+    updateValue(data.gets.avgTime, "avg-get-time", true, 2, true);
+    updateValue(data.gets.medTime, "med-get-time", true, 2, true);
+    updateValue(data.gets.longest.length, "longest-get", true, 2, true);
+    $("#longest-get-direction").html(data.gets.longest.begin < data.gets.longest.end ? "up" : "down")
+    updateValue(data.gets.longest.begin, "longest-get-begin");
+    updateValue(data.gets.longest.end, "longest-get-end");
+    updateValue(data.gets.avgLen, "avg-get-len", true, 2);
+    updateValue(data.gets.medLen, "med-get-len", true);
+    updateValue(data.gets.lastLen, "last-get-len");
+    updateValue(data.getSign.maxUpStreak.length, "max-up-get-streak", true);
+    updateValue(data.getSign.maxUpStreak.begin, "max-up-get-streak-begin");
+    updateValue(data.getSign.maxUpStreak.end, "max-up-get-streak-end");
+    updateValue(data.getSign.maxDownStreak.length, "max-down-get-streak", true);
+    updateValue(data.getSign.maxDownStreak.begin, "max-down-get-streak-begin");
+    updateValue(data.getSign.maxDownStreak.end, "max-down-get-streak-end");
+    updateValue(data.getSign.avgStreak, "avg-get-streak", true);
+    updateValue(data.getSign.posNegAsym, "pos-neg-get-asym", true);
+    updateValue(data.getSign.pos, "pos-gets", true);
+    updateValue(data.getSign.neg, "neg-gets", true);
+    updateValue(data.getSign.posPercent, "pos-gets-percent", true, 2);
+    updateValue(data.getSign.negPercent, "neg-gets-percent", true, 2);
+    updateValue(data.getSign.zeros, "zero-gets", true);
+    updateValue(data.getSign.ofZero, "of-gets-zero", true, 2);
+    updateValue(data.getSign.maxPosStreak, "max-pos-get-streak", true);
+    updateValue(data.getSign.maxNegStreak, "max-neg-get-streak", true);
+    
+    updateTableNR(data.oneStalemates, "one-stalemates");
+    updateTableNR(data.twoStalemates, "two-stalemates", true);
+    
+    updateValue(data.deletedCounts, "deleted-counts", true);
+    updateValue(data.forks, "forks", true);
+    updateValue(data.users.count, "total-usrs", true);
+    updateValue(data.users.avg, "avg-usr", true, 2);
+    updateValue(data.users.med, "med-usr", true);
+    
+    updateTableR(data.users.top20, "top-20-usrs");
+    updateTableR(data.users.topGets, "top-usr-gets");
+    
+    updateValue(data.users.topGets.threshold, "usr-min-gets");
+    
+    updateTableR(data.users.topAssists, "top-usr-assists");
+    
+    updateValue(data.users.topAssists.threshold, "usr-min-assists");
+    
+    updateTableR(data.users.fastest, "top-usr-speed");
+    
+    updateValue(data.users.fastest.threshold, "usr-min-counts");
+    
+    updateTableR(data.users.fastestMed, "top-usr-med-speed");
+    updateTableR(data.users.speedScore, "top-usr-speed-score");
+    updateTableR(data.users.topTeamScore, "top-team-score-_and");
+    
+    updateTableR(data.top100, "top-100-usrs");
+});
