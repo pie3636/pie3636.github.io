a673abd523a72db1ded4f8d16fa1031892881ea2
Maxime Meloux <maxime.meloux@protonmail.com>
2020-02-18 23:57:48 +0100

    Fixing merge conflict

--- tower-of-hero/main.js
+++ tower-of-hero/main.js
@@ -437,73 +437,6 @@ function simulateChest(curItems, floor, noBuy) {
             }
         }
     }
-<<<<<<< HEAD
-    lv = curItems[itemID1];
-    switch (itemID1) {
-        case 3:
-            if (lv >= 15 && randomBool(engine)
-                || lv >= 21 && random40Bool(engine)
-                || lv >= 24 && random30Bool(engine)
-                || lv >= 27 && random30Bool(engine)) {
-                itemID1 = 1;
-            }
-            break;
-        case 4:
-            if (lv > 9 && (flag1 || flag3 || floor < 151 + 40 * lv)) {
-                itemID1 = 2;
-            }
-            break;
-        case 6:
-            if (lv > 20 && (flag1 || flag3 || floor < 301)
-                || lv >= 15 && randomBool(engine)
-                || lv >= 18 && randomBool(engine)
-                || lv >= 22 && randomBool(engine)) {
-                itemID1 = 1;
-            }
-            break;
-        case 10:
-        case 12:
-        case 14:
-        case 16:
-            if (lv >= 210 && random70Bool(engine)) {
-                itemID1 = 2;
-            }
-            break;
-        case 21:
-        case 22:
-            num3 = 100 + 36 * lv + (lv < 3 ? 0 : 100) + (lv < 5 ? 0 : 70) + (lv < 10 ? 0 : 90) + (lv < 25 ? 0 : 80) + (lv < 35 ? 0 : 80);
-            if (num3 > 1199) {
-                num3 = num3 * 0.5 + 599.5;
-            }
-            if (num3 > 1499) {
-                num3 = num3 * 0.5 + 749.5;
-            }
-            if (num3 > 1899) {
-                num3 = num3 * 0.5 + 949.5;
-            }
-            if (flag1 || flag2 || floor < num3 + 1 || lv >= 25 && random30Bool(engine)) {
-                itemID1 = 2;
-            }
-            break;
-        case 23:
-            if (flag1
-                || flag3
-                || lv <= 0 && floor < 251
-                || lv == 1 && floor < 301
-                || lv >= 2 && floor < 351 + lv * 35 + (lv < 6 ? 0 : 100)
-                || lv >= 91 && random80Bool(engine)
-                || lv >= 20 && random01(engine) > 0.09994 + floor * 0.00006
-                || lv >= 16 && random01(engine) > 0.19992 + floor * 0.00008
-                || lv >= 12 && random01(engine) > 0.2999 + floor * 0.0001
-                || lv >= 8 && random01(engine) > 0.4999 + floor * 0.0001
-                || lv >= 5 && random01(engine) > 0.7999 + floor * 0.0001
-                || floor < 1000 &&
-                    (lv >= 20 && random30Bool(engine)
-                    || lv >= 40 && random30Bool(engine)
-                    || lv >= 60 && random30Bool(engine)
-                    || lv >= 80 && random30Bool(engine))
-            ) {
-=======
     lv2 = curItems[itemID1];
     switch (itemID1) {
         case 3:
@@ -557,123 +490,24 @@ function simulateChest(curItems, floor, noBuy) {
             break;
         case 23:
             if (flag1 || flag3 || lv2 <= 0 && floor < 251 || lv2 == 1 && floor < 301 || lv2 >= 2 && floor < 351 + lv2 * 35 + (lv2 < 6 ? 0 : 100)) {
->>>>>>> e6c97c722b9436704dd876dbda8a74103e35b42d
                 itemID1 = 2;
             }
-<<<<<<< HEAD
-            break;
-        case 25:
-            if (flag1
-                || flag3
-                || floor < (111 + 90 * lv + lv * lv * 6 + (lv < 7 ? 0 : 150)) + (lv < 9 ? 0 : 90 + (lv * lv) * 0.5)
-                || lv >= 9 && randomBool(engine)) {
-=======
             if (lv2 >= 91 && random80Bool(engine)) {
->>>>>>> e6c97c722b9436704dd876dbda8a74103e35b42d
                 itemID1 = 2;
             }
-<<<<<<< HEAD
-            break;
-        case 27:
-        case 28:
-            if (flag1
-                || flag2
-                || floor < 133 + 33 * lv + (lv < 3 ? 0 : 100) + (lv < 5 ? 0 : 80) + (lv < 10 ? 0 : 90) + (lv < 25 ? 0 : 90) + (lv < 35 ? 0 : 80)
-                || lv >= 25 && random30Bool(engine)) {
-=======
             if (lv2 >= 20 && random01(engine) > 0.09994 + floor * 0.00006) {
->>>>>>> e6c97c722b9436704dd876dbda8a74103e35b42d
                 itemID1 = 2;
             }
-<<<<<<< HEAD
-            break;
-        case 31:
-        case 32:
-            if (floor <= 600) {
-                itemID1 -= 2;
-            }
-        case 33:
-            if (floor < 20 * lv * (lv - 1) - 99) {
-=======
             if (lv2 >= 16 && random01(engine) > 0.19992 + floor * 0.00008) {
                 itemID1 = 2;
                 break;
             }
             if (lv2 >= 12 && random01(engine) > 0.2999 + floor * 0.0001) {
->>>>>>> e6c97c722b9436704dd876dbda8a74103e35b42d
                 itemID1 = 2;
             }
-<<<<<<< HEAD
-            break;
-        case 34:
-        case 35:
-            if (flag1
-                || flag3
-                || lv <= 0 && floor < 351
-                || lv >= 1 && floor < 461 + lv * 40
-                || lv >= 2 && random01(engine) < 0.30005 - floor * 0.00005
-                || lv >= 3 && random01(engine) < 0.40005 - floor * 0.00005
-                || lv >= 5 && random01(engine) < 0.50005 - floor * 0.00005
-                || lv >= 10 && random01(engine) < 0.50005 - floor * 0.00005
-                || lv >= 20 && random01(engine) < 0.50005 - floor * 0.00005
-                || lv >= 45 && random01(engine) < 0.50005 - floor * 0.00005
-                || floor < 901 &&
-                    (lv >= 48 && random40Bool(engine)
-                    || lv >= 58 && random40Bool(engine)
-                    || lv >= 68 && random40Bool(engine))
-                || floor < 1101 &&
-                    (lv >= 48 && random30Bool(engine)
-                    || lv >= 58 && random30Bool(engine)
-                    || lv >= 68 && random30Bool(engine))) {
-=======
             if (lv2 >= 8 && random01(engine) > 0.4999 + floor * 0.0001) {
->>>>>>> e6c97c722b9436704dd876dbda8a74103e35b42d
                 itemID1 = 2;
             }
-<<<<<<< HEAD
-            break;
-        case 37:
-        case 38:
-            if (curItems[itemID1 - 3] == 0
-                || flag1
-                || flag3
-                || (lv <= 0 && floor < 501 || lv >= 1 && floor < 621 + lv * 46)
-                || lv >= 2 && random01(engine) < 0.40005 - floor * 0.00005
-                || lv >= 3 && random01(engine) < 0.50005 - floor * 0.00005
-                || lv >= 5 && random01(engine) < 0.70005 - floor * 0.00005
-                || lv >= 10 && random01(engine) < 0.50005 - floor * 0.00005
-                || lv >= 20 && random01(engine) < 0.50005 - floor * 0.00005) {
-                itemID1 = 2;
-            } else if (lv >= 20 && randomBool(engine)
-                       || lv >= 25 && random25Bool(engine)
-                       || lv >= 28 && random25Bool(engine)) {
-                itemID1 = 1;
-            } else if (floor < 1401 &&
-                           (lv >= 22 && random40Bool(engine)
-                           || lv >= 24 && random40Bool(engine)
-                           || lv >= 26 && random40Bool(engine))
-                       || floor < 1601 &&
-                           (lv >= 22 && random30Bool(engine)
-                           || lv >= 24 && random30Bool(engine)
-                           || lv >= 26 && random30Bool(engine))
-                       ) {
-                itemID1 = 2;
-            }
-            break;
-        case 39:
-            if (lv != 0) {
-                num2 = 0;
-                if (lv >= 10) {
-                    num2 = 201 + lv * 44;
-                }
-                if (lv >= 5) {
-                    num2 = 20 + lv * 8;
-                }
-                if (floor < 181 + lv * 36 + num2
-                    || lv >= 10 && random45Bool(engine)
-                    || lv >= 12 && random45Bool(engine)
-                    || lv >= 14 && random45Bool(engine)) {
-=======
             if (lv2 >= 5 && random01(engine) > 0.7999 + floor * 0.0001) {
                 itemID1 = 2;
                 break;
@@ -692,164 +526,18 @@ function simulateChest(curItems, floor, noBuy) {
                     break;
                 }
                 if (lv2 >= 80 && random30Bool(engine)) {
->>>>>>> e6c97c722b9436704dd876dbda8a74103e35b42d
                     itemID1 = 2;
                 }
             }
             break;
-<<<<<<< HEAD
-        case 40:
-            if (floor < 251 + 39 * lv) {
-                itemID1 = 1;
-            }
-            break;
-        case 41:
-            if (curItems[40] == 0
-                || floor < 291 + 190 * lv
-                || lv >= 3 && randomBool(engine)) {
-                itemID1 = 1;
-            }
-            break;
-        case 42:
-            if (curItems[40] == 0 || floor < 221 + 2.4 * lv) {
-                itemID1 = 1;
-            }
-            if (lv >= 350 && random30Bool(engine)
-                || lv >= 500 && random40Bool(engine)
-                || lv >= 600 && randomBool(engine)) {
-=======
         case 25:
             if (flag1 || flag3 || floor < (111 + 90 * lv2 + lv2 * lv2 * 6 + (lv2 < 7 ? 0 : 150)) + (lv2 < 9 ? 0 : 90 + (lv2 * lv2) * 0.5)) {
->>>>>>> e6c97c722b9436704dd876dbda8a74103e35b42d
                 itemID1 = 2;
             }
-<<<<<<< HEAD
-            break;
-        case 43:
-            if (curItems[40] == 0 || curItems[42] == 0 || floor < 321 + 4.6 * lv) {
-                itemID1 = 1;
-            }
-            if (lv >= 200 && random40Bool(engine) || lv >= 300 && randomBool(engine)) {
-=======
             if (lv2 >= 9 && randomBool(engine) < 0.5) {
->>>>>>> e6c97c722b9436704dd876dbda8a74103e35b42d
                 itemID1 = 2;
             }
             break;
-<<<<<<< HEAD
-        case 44:
-            if (lv >= 1 && floor < 101 + 4 * lv + (lv < 5 ? 0 : 50) + (lv < 10 ? 0 : 70) + (lv < 40 ? 0 : 70) + (lv < 60 ? 0 : 70) + (lv < 70 ? 0 : 70)) {
-                itemID1 = 2;
-            }
-            break;
-        case 45:
-            if (floor < 451 + 6 * lv + (lv >= 70 ? 100 : 0) + (lv >= 80 ? 200 : 0) + (lv >= 90 ? 200 : 0)) {
-                itemID1 = 1;
-            }
-            break;
-        case 48:
-            if (lv >= 1 && (flag3 || floor < 241 + 41 * lv)
-                || floor < 141) {
-                itemID1 = 2;
-            }
-            break;
-        case 49:
-            if (flag3 || floor < 231 + 45 * lv) {
-                itemID1 = 2;
-            }
-            break;
-        case 50:
-            if (flag3 || floor < 211 + 49 * lv) {
-                itemID1 = 2;
-            }
-            break;
-        case 51:
-            if (lv >= 1 && floor < 201 + 35 * lv
-                || floor < 161
-                || lv >= 24 && random30Bool(engine)
-                || lv >= 26 && random30Bool(engine)) {
-                itemID1 = 2;
-            }
-            break;
-        case 52:
-            if (curItems[40] == 0 || curItems[43] <= 10 || floor < 501 + 12 * lv) {
-                itemID1 = 1;
-            }
-            if (lv >= 25 && random40Bool(engine)
-                || lv >= 50 && random30Bool(engine)
-                || lv >= 75 && random40Bool(engine)) {
-                itemID1 = 2;
-            }
-            break;
-        case 53:
-            if (flag1
-                || flag3
-                || floor < 761 + 5 * lv
-                || lv >= 2 && random10Bool(engine)
-                || lv >= 3 && random30Bool(engine)) {
-                itemID1 = 2;
-            } else if (lv >= 10 && random15Bool(engine) || lv >= 50 && random25Bool(engine)) {
-                itemID1 = 1;
-            } else if (lv >= 80 && random25Bool(engine)) {
-                itemID1 = 2;
-            }
-            break;
-        case 54:
-            if (lv <= 14) {
-                itemID1 = 2;
-            }
-            if (lv <= 15 && random70Bool(engine)) {
-                itemID1 = 1;
-            }
-            lv = curItems[itemID1];
-            if (floor < 851 + lv * 80) {
-                itemID1 = 1;
-            } else if (flag2 || lv >= 1 && random40Bool(engine) || lv >= 3 && random60Bool(engine)) {
-                itemID1 = 2;
-            } else if (lv >= 5 && random40Bool(engine) || lv >= 6 && random90Bool(engine)) {
-                itemID1 = 1;
-            } else if (lv >= 7 && random40Bool(engine) || lv >= 9 && random90Bool(engine)) {
-                itemID1 = 2;
-            }
-            if (itemID1 == 1 || itemID1 == 2) {
-                itemID1 = random75Bool(engine) ? 2 : 1;
-            }
-            break;
-        case 55:
-            if ((lv >= 2 && random30Bool(engine)
-                || lv >= 4 && random70Bool(engine)
-                || lv >= 5 && random90Bool(engine)
-                || lv >= 7 && random90Bool(engine)
-                || flag3)) {
-                itemID1 = random75Bool(engine) ? 2 : 1;
-            }
-            break;
-        case 56:
-            lv2 = curItems[47];
-            if (lv2 < 500
-                || lv2 < lv * 500
-                || flag3 && random30Bool(engine)
-                || floor < 201) {
-                itemID1 = 2;
-            }
-            break;
-        case 57:
-            if (lv == 0 && (random90Bool(engine) && (flag3 || floor < 150) || floor < 226)
-                || flag2
-                || floor < 801 + lv * 200) {
-                itemID1 = 2;
-            }
-            break;
-        case 58:
-            if (flag2 || floor < 1651 + lv * 200) {
-                itemID1 = 2;
-            }
-            break;
-        case 59:
-            if (flag2 || floor < 1151 + lv * 150) {
-                itemID1 = 2;
-            }
-=======
         case 27:
         case 28:
             num3 = 132 + 33 * lv2 + (lv2 < 3 ? 0 : 100) + (lv2 < 5 ? 0 : 80) + (lv2 < 10 ? 0 : 90) + (lv2 < 25 ? 0 : 90) + (lv2 < 35 ? 0 : 80);
@@ -1136,7 +824,6 @@ function simulateChest(curItems, floor, noBuy) {
                 itemID1 = 2;
             else if (lv2 >= 20 && random25Bool(engine))
                 itemID1 = 2;
->>>>>>> e6c97c722b9436704dd876dbda8a74103e35b42d
             break;
     }
     
@@ -1264,13 +951,9 @@ function simulateRun(curItems, beginFloor, endFloor) {
         if (cur % 5 == 0) {
             chests[cur] = 1;
             if (cur % 10 == 0) {
-<<<<<<< HEAD
-                if (random25Bool(engine)) {
-=======
                 if (endFloor < cur / 10) // TODO record floor instead
                     chests[cur] = cur != 20 ? (cur % 50 != 0 ? 3 : 4) : 2;
                 else if (random25Bool(engine))
->>>>>>> e6c97c722b9436704dd876dbda8a74103e35b42d
                     chests[cur] = 2;
                 }
                 else if (random01(engine) < 0.22 && cur % 50 == 0) {
@@ -1936,11 +1619,7 @@ function debugInit() {
 }
 
 function debugCur() { // /u/Vetokend 20190526
-<<<<<<< HEAD
     items = [20,12273,19516,30,20,15123,25,9643,100,7729,300,10,300,10,300,10,300,10,600,11257,10,105,105,71,6004,12,3,45,45,13377,15323,12111,15335,10,70,70,13014,30,30,16,31,5,701,401,76,100,355,512,16,16,11,30,151,118,10,13,3,3,2,3]
-=======
-    items = [20, 11675, 16519, 30, 10, 13903, 25, 8962, 100, 7087, 300, 10, 300, 10, 300, 10, 300, 10, 600, 10489, 10, 105, 105, 69, 5540, 12, 3, 45, 45, 12853, 13986, 11077, 15100, 10, 70, 70, 12271, 30, 30, 16, 31, 5, 701, 401, 76, 100, 355, 460, 16, 16, 11, 30, 151, 86, 10, 11, 1, 3, 0]
->>>>>>> e6c97c722b9436704dd876dbda8a74103e35b42d
     j = 0;
     for (i in itemObjs) {
         $("#" + nameCleanup(i))[0].value = items[j];
@@ -2044,21 +1723,13 @@ function doImportPreset(toImport) {
     items = toImport[0];
     var j = 0;
     for (i in itemObjs) { // Update item levels in GUI
-<<<<<<< HEAD
-=======
         if (i == "Coat of Gold") i = "Golden Coat"; // Pre-2.0.0 compatibility
         if (i == "A King's Crown") i = "Royal Crown";
->>>>>>> e6c97c722b9436704dd876dbda8a74103e35b42d
         val = items[j];
         if (val === undefined) {
             val = 0;
         }
-<<<<<<< HEAD
-        $("#" + nameCleanup(i))[0].value = val;
-        $("#" + nameCleanup(i) + "_i")[0].value = val;
-=======
         $("#" + i.toLowerCase().replace(/ /g, "_").replace(/'/g, "").replace(/\+/g, ""))[0].value = val;
->>>>>>> e6c97c722b9436704dd876dbda8a74103e35b42d
         j++;
     }
     $("#" + toImport[1]).prop("checked", true).trigger("click");
@@ -2172,7 +1843,6 @@ function doRemovePreset() {
     updatePresets();
 }
 
-<<<<<<< HEAD
 function changeTab(newTab) {
         $('#nav_' + currentTab).parent().removeClass('active');
         $('#nav_' + newTab).parent().addClass('active');
@@ -2181,42 +1851,12 @@ function changeTab(newTab) {
         currentTab = newTab;
         scroll();
 }
-=======
-$(function() {
-
-    $('[data-toggle="tooltip"]').tooltip();
-    $("#version").append(versionStr);
-
-    engine = Random.engines.mt19937().autoSeed();
-    random01 = Random.real(0, 1, false);
-    randomBool = Random.bool();
-    random10Bool = Random.bool(.1);
-    random15Bool = Random.bool(.15);
-    random20Bool = Random.bool(.2);
-    random25Bool = Random.bool(.25);
-    random30Bool = Random.bool(.3);
-    random40Bool = Random.bool(.4);
-    random45Bool = Random.bool(.45);
-    random60Bool = Random.bool(.6);
-    random70Bool = Random.bool(.7);
-    random75Bool = Random.bool(.75);
-    random80Bool = Random.bool(.8);
-    random85Bool = Random.bool(.85);
-    random90Bool = Random.bool(.9);
-
-    itemCount = 0;
->>>>>>> e6c97c722b9436704dd876dbda8a74103e35b42d
 
 function copyItemsLoot() {
     for (var i in itemObjs) {
         $("#" + nameCleanup(i))[0].value = $("#" + nameCleanup(i) + "_i")[0].value;
     }
-<<<<<<< HEAD
 }
-=======
-
-    items = new Array(itemCount).fill(0); // TODO
->>>>>>> e6c97c722b9436704dd876dbda8a74103e35b42d
 
 function copyLootItems() {
     for (var i in itemObjs) {
@@ -2225,16 +1865,7 @@ function copyLootItems() {
     for (var i in itemObjs) {
         updateItemEffect(i);
     }
-<<<<<<< HEAD
 }
-=======
-
-    // Event listeners (must be done before loading autosave)
-    $("#full-run").click(function() {
-        $("#run-form").show();
-        $("#one-floor").hide();
-    });
->>>>>>> e6c97c722b9436704dd876dbda8a74103e35b42d
 
 function getTable(strCaption, strData, start, end, fCaption, fData, cl="") {
     str = "<table class='table table-hover table-data'><thead><tr><th>" + strCaption + "</th>";
@@ -2256,20 +1887,9 @@ function rnd(x, n) {
     return +x.toFixed(n)
 }
 
-<<<<<<< HEAD
 function getAnchor(id) {
     return "<a class='alink' href='#" + nameCleanup(id) + "_l'><samp>" + id + "</samp></a>";
 }
-=======
-    $("#normal-quest, #always-quest").click(function() {
-        $("#quest-form").show();
-    });
-
-    var autosave = localStorage.getItem("_autosave");
-    if (autosave) {
-        doImportPreset(JSON.parse(RawDeflate.inflate(Base64.decode(autosave))));
-    }
->>>>>>> e6c97c722b9436704dd876dbda8a74103e35b42d
 
 function getTime(x) {
     x = rnd(x, 2);
@@ -2355,7 +1975,6 @@ function getEffect(id, lv = 0) {
     }
 }
 
-<<<<<<< HEAD
 Object.defineProperties(Array.prototype, {
     'collapseSimple': {
         value: function() {
@@ -2398,25 +2017,6 @@ Object.defineProperties(Array.prototype, {
             this[this.length - 1] = newProb;
         }
     }
-=======
-    // Event listeners
-    $("#simulate").click(runSimulation);
-    $("#prob, #avg").click(displayResults);
-    $("#btn-save").click(savePreset);
-    $("#btn-import").click(openImport);
-    $("#btn-export").click(exportPreset);
-    $("#do-import").click(importPreset);
-    $("#do-save").click(doSavePreset);
-    $("#do-remove").click(doRemovePreset);
-    $("#btn-load").click(loadPreset);
-    $("#btn-remove").click(removePreset);
-
-    saveInterval = setInterval(function() {
-        localStorage.setItem("_autosave", getExportData());
-    }, 1000);
-
-    //debugCur();
->>>>>>> e6c97c722b9436704dd876dbda8a74103e35b42d
 });
 
 function simulateChestProba(lv, claymoreLv, resets, endFloor) {
